163dd34be49ef8717bbb708f29a79d7d7a8924a1
kiat <kiat.mao@gmail.com>
2023-12-13 14:38:42 +0800

    add from_jbda_file

--- app/models/file_helper.rb
+++ app/models/file_helper.rb
@@ -147,4 +147,29 @@ class FileHelper
     start_date = Date.yesterday
     Express.from_zh_second_file_by_date(start_date)
   end
+
+  # 
+  def self.from_jbda_file
+    direct_r = I18n.t("orders_r_path")
+    if !File.exist?(direct_r)
+      Dir.mkdir(direct_r)          
+    end
+
+    download_dir = "/#{FileHelper::FTP_CONFIG[:jbda_dir]}"
+
+    Net::SFTP.start(FileHelper::FTP_CONFIG[:ftp_ip], FileHelper::FTP_CONFIG[:username], :password => FileHelper::FTP_CONFIG[:password]) do |sftp|
+      sftp.dir.foreach("#{download_dir}") do |entry|
+        if entry.name.start_with? I18n.t("jbda_file_name")
+          sftp.download!("#{download_dir}/#{entry.name}", "#{direct_r}/#{entry.name}")
+
+          Rails.logger.info("#{Time.now} jbda_file_name download #{entry.name}")
+
+          sftp.remove!("#{download_dir}/#{entry.name}")
+        end
+      end
+    end
+    
+    start_date = Date.today
+    Order.get_orders_by_date(start_date)
+  end
 end
