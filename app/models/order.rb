014c3b831186373659af88f371385edc307c084a
yyy <kiat.mao@gmail.com>
2023-12-19 10:05:38 +0800

    fix conflict

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,54 +5,63 @@ class Order < ApplicationRecord
   enum address_status: {address_waiting: 'address_waiting', address_success: 'address_success', address_failed: 'address_failed', address_noparse: 'address_noparse', address_parseing: 'address_parseing'}
   ADDRESS_STATUS_NAME = {address_waiting: '待解析', address_success: '解析成功', address_failed: '解析失败', address_noparse: '不解析', address_parseing: '解析中'}
 
+  def status_name
+    status.blank? ? "" : Order::STATUS_NAME["#{status}".to_sym]
+  end
+
   def address_status_name
     address_status.blank? ? "" : Order::ADDRESS_STATUS_NAME["#{address_status}".to_sym]
   end
 
   def self.get_orders_by_date(start_date)
-  	to_deal_r_files = []
+  	to_deal_r_files = []   #加密压缩文件
   	fdate = start_date.strftime('%Y%m%d')
 
-  	direct_r = I18n.t("orders_r_path")
-  	if !File.exist?(direct_r)
-	    Dir.mkdir(direct_r)          
+  	direct = I18n.t("orders_r_path")
+  	if !File.exist?(direct)
+	    Dir.mkdir(direct)          
 		end
-  
-		fname_starts = I18n.t("download_file_start")
-  	
-  	Dir.children(direct_r).each do |f|
-  		fname_starts.each do |s|
-  			fname_start= s+"MAIL"+fdate
-	  		if f.start_with? fname_start
+
+    fname_starts = I18n.t("jbda_file_name")
+    if !fname_starts.blank?
+      fname_start= fname_starts+'_'+fdate
+    
+    	Dir.children(direct).each do |f|
+    		if f.start_with? fname_start
 	  			to_deal_r_files << f
 	  		end
-	  	end
-  	end
+  	  end
 
-  	to_deal_r_files.each do |ff|
-  		file_path_r = File.join(direct_r, ff)
-      Order.get_orders(file_path_r)
-  	end
+    	to_deal_r_files.each do |ff|
+        #加密压缩文件路径
+    		file_path_r_encrypt = File.join(direct, ff)   
+        Order.get_orders(file_path_r_encrypt)
+    	end
+    end
   end
 
-  def self.get_orders(file_path_r)
+  def self.get_orders(file_path_r_encrypt)
     ActiveRecord::Base.transaction do
-      direct_t = I18n.t("orders_t_path")
-      if !File.exist?(direct_t)
-        Dir.mkdir(direct_t)          
-      end
+      # 解密文件
+      FileHelper.gpg_decrypt_file("888888888888", file_path_r_encrypt, nil)
+      #解密压缩文件路径
+      file_path_r_zip = file_path_r_encrypt.gsub(File.extname(file_path_r_encrypt), '')  
+      # 解压文件
+      unzip_decrypt_files = FileHelper.extract_rar(file_path_r_zip, nil)
+      #解密解压文件夹路径
+      file_path_r_unzip = file_path_r_zip.gsub(File.extname(file_path_r_zip), '')  
 
-      file_name_r = file_path_r.split("/").last
+      Dir.children(file_path_r_unzip).each do |f|
+        # 数据文件路径
+        to_deal_file_path = File.join(file_path_r_unzip, f) 
 
-      file_path_t = File.join(direct_t, "decrypt_#{file_name_r}")
-      FileHelper.sm4_decrypt_file("EMWL888888888888", file_path_r, file_path_t)
-
-      File.open(file_path_t, "r:UTF-8") do |file|
-      # File.open(file_path_r, "r:UTF-8") do |file|
-      	sender_name = Order.get_sender_name(file_name_r)
-        file.each_line do |line|
-          columns = line.split("!^?")
-          Order.create! express_no: columns[0], receiver_postcode: columns[1], receiver_addr: columns[2], receiver_name: columns[3], receiver_phone: columns[5], sender_province: "上海", sender_city: "上海市", sender_district: "浦东新区", sender_addr: "上海邮政信箱120-058", sender_name: sender_name, sender_phone: "4008205555", status: "waiting", address_status: "address_waiting"
+        File.open(to_deal_file_path, "r:UTF-8") do |file|
+          # sender_name = Order.get_sender_name(File.basename(to_deal_file_path))
+        	sender_name = "招行卡中心"  
+          file.each_line do |line|
+            columns = line.split("!^?")
+            Order.create! express_no: columns[0], receiver_postcode: columns[1], receiver_addr: columns[2], receiver_name: columns[3], receiver_phone: columns[5], sender_province: "上海", sender_city: "上海市", sender_district: "浦东新区", sender_addr: "上海邮政信箱120-058", sender_name: sender_name, sender_phone: "4008205555", status: "waiting", address_status: "address_waiting"
+          end
         end
       end
     end
